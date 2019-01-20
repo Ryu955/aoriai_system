ca4ef783fb4502803ff816dec2568788fe5349fa
Ryu955 <c0114046aa@edu.teu.ac.jp>
2019-01-21 00:14:24 +0900

    値がmapでとれるようになった

--- collect/collect_gitlog.go
+++ collect/collect_gitlog.go
@@ -6,7 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 		"time"
-	)
+	"sort"
+)
 
 type GitLog []struct {
 	Sha    string `json:"sha"`
@@ -112,13 +113,52 @@ func GetGitLog(repo_name string) {
 		return
 	}
 
+	// commit_map := make(map[string]int)
+	commit_map := make(map[string]int)
 	jst, _ := time.LoadLocation("Asia/Tokyo")
-
 	for count, log := range gl {
 		fmt.Println(count)
 		commit_date := log.Commit.Author.Date.In(jst).Format("2006-01-02")
 		fmt.Println(commit_date)
+
+		_, is_exist	:= commit_map[commit_date]
+		if is_exist {
+			commit_map[commit_date] = commit_map[commit_date] + 1
+		} else {
+			commit_map[commit_date] = 1
+		}
 	}
-	// fmt.Println(string(body))
+
+
+	fmt.Println(commit_map)
+
+	a := List{}
+	for k, v := range commit_map {
+		e := Entry{k, v}
+		a = append(a, e)
+	}
+
+	sort.Sort(a)
+	fmt.Println(a)
+}
+type Entry struct {
+	name  string
+	value int
+}
+type List []Entry
+
+func (l List) Len() int {
+	return len(l)
 }
 
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l List) Less(i, j int) bool {
+	if l[i].value == l[j].value {
+		return (l[i].name < l[j].name)
+	} else {
+		return (l[i].value < l[j].value)
+	}
+}
